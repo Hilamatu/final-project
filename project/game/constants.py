72a13be0df640fc5264c9460f6c85a4c413a9088
msander87 <90715788+msander87@users.noreply.github.com>
2021-12-05 02:57:38 -0300

    Update constants.py

--- project/game/constants.py
+++ project/game/constants.py
@@ -1,10 +1,19 @@
+
+ROUNDS = 51
+SCALING = 1
+SPRITE_SIZE = 90
+
+
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
-SCREEN_TITLE = "Skater"
-SCALING = 1
-GROUND_LEVEL = 200
+TITLE = "Skater"
 
-SPRITE_SIZE = 120
-
-JUMP_SPEED = 20
+SPEED = -10
+JUMP_SPEED = 13
 GRAVITY = .9 
+
+#sounds
+SOUND_CRASH = "skater/sounds/crash.wav"
+SOUND_OUCH = "skater/sounds/ouch.wav"
+SOUND_JUMP = "skater/sounds/jump.wav"
+
